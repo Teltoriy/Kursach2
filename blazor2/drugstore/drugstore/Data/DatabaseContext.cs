2a0746aa3ab151e237c6658fdda3a8946dbdda7a
Teltoriy <boevoygorox@gmai.com>
2024-05-20 21:56:12 +0300

    main logic added

---
+++ blazor2/drugstore/drugstore/Data/DatabaseContext.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Drugstore.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<pharmacy> pharmacy { get; set; }
+    public DbSet<product> product { get; set; }
+    public DbSet<supplier> supplier { get; set; }
+    public DbSet<pharmacysupplier> pharmacysupplier { get; set; }
+    public DbSet<pharmacyproduct> pharmacyproduct { get; set; }
+    public DbSet <suplierproduct> suplierproduct { get; set; }
+    public DbSet<productpackingpharma> productpackingpharma { get; set; }
+    public DbSet<productpacking> productpacking { get; set; }
+    public DbSet<productpackingsupplier> productpackingsupplier { get; set; }
+
+    public DatabaseContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=Roflanxlebalo;Database=pharma;Port=5432");
+    }
+}
+public class pharmacy //для аптек
+{
+    [Key]
+    public int id { get; set; }
+    public string name { get; set; }
+    public DateTime date { get; set; }
+    public List<pharmacysupplier>? PharmacySuppliers { get; set; }
+    public List<pharmacyproduct>? PharmacyProducts { get; set; }
+}
+
+public class product //для продуктов
+{
+    [Key]
+    public int id { get; set; }
+    public string name { get; set; }
+    public List<productpackingpharma> packingPharma { get; set; }
+    public decimal cost { get; set; }
+    public int quantity { get; set; }
+    public DateTime expirationdate { get; set; }
+    public int favouritesupplierid { get; set; }
+    public supplier favouritesupplier { get; set; }
+    public List<suplierproduct> supplierproducts { get; set; }
+    public List<productpacking> packing { get; set; }
+    public List<productpackingsupplier> packingSupplier { get; set; }
+
+
+}
+public class productpackingpharma //массовка для аптеки
+{
+    [Key]
+    public int id { get; set; }
+    public int productid { get; set; }
+    public int packingid { get; set; }
+    public int quantity { get; set; }
+    public product product { get; set; }
+    public productpacking packing { get; set; }
+}
+
+public class supplier //поставщики
+{
+    [Key]
+    public int id { get; set; }
+    public string name { get; set; }
+    public List<suplierproduct> supplierproducts { get; set; }
+}
+public class pharmacysupplier // Поставщик для аптеки
+{
+    [Key]
+    public int id { get; set; }
+    public int pharmacyid { get; set; }
+    public int supplierid { get; set; }
+    public pharmacy pharmacy { get; set; } 
+    public supplier supplier { get; set; }
+}
+
+public class pharmacyproduct //Товары в аптеке
+{
+    [Key]
+    public int id { get; set; }
+    public int pharmacyid { get; set; }
+    public int productid { get; set; }
+    public pharmacy Pharmacy { get; set; } 
+    public product product { get; set; }
+}
+public class suplierproduct //Товары поставщика 
+{
+    [Key]
+    public int id { get; set; }
+    public int supplierid { get; set; }
+    public int productid { get; set; }
+    public supplier supplier { get; set; }
+    public product product { get; set; }
+}
+public class productpackingsupplier //Массовка для товаров поставщика
+{
+    [Key]
+    public int id { get; set; }
+    public int productid { get; set; }
+    public int packingid { get; set; }
+    public int quantity { get; set; }
+    public product product { get; set; }
+    public productpacking packing { get; set; }
+}
+public class productpacking //Возможная массовка товара
+{
+    [Key]
+    public int id { get; set; }
+    public int productid { get; set; }
+    public string packing { get; set; }
+    public product product { get; set; }
+    public List<productpackingpharma> productpackingpharmas { get; set;  }
+    public List<productpackingsupplier> pps { get; set; }
+}
---
+++ blazor2/drugstore/drugstore/Service/Service.cs
@@ -0,0 +1,170 @@
+﻿using Drugstore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+
+namespace Drugstore.Service;
+public class PharmacyService(DatabaseContext context)
+{
+    private readonly DatabaseContext _context = context;
+    public async Task<IDbContextTransaction> BeginTransactionAsync()
+    {
+        return await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task<List<pharmacy>> GetPharmaciesAsync()
+    {
+        return await _context.pharmacy.ToListAsync();
+    }
+
+    public async Task<pharmacy> GetPharmacyByIdAsync(int id)
+    {
+        var pharmacy = await _context.pharmacy
+            .Include(p => p.PharmacyProducts)
+                .ThenInclude(pp => pp.product)
+                .ThenInclude(p=>p.packing)
+                .ThenInclude(p => p.productpackingpharmas)
+            .Include(p => p.PharmacySuppliers)
+                .ThenInclude(ps => ps.supplier)
+            .FirstOrDefaultAsync(p => p.id == id);
+
+        return pharmacy;
+    }
+    public async Task RemoveProductAsync(int productId)
+    {
+        // Найти все записи в pharmacyproduct, связанные с продуктом
+        var pharmacyProducts = await _context.pharmacyproduct
+            .Where(pp => pp.productid == productId)
+            .ToListAsync();
+
+        if (pharmacyProducts != null)
+        {
+            _context.pharmacyproduct.RemoveRange(pharmacyProducts);
+        }
+
+        // Найти все записи в productpacking, связанные с продуктом
+        var productPackings = await _context.productpackingpharma
+            .Where(pp => pp.productid == productId)
+            .ToListAsync();
+
+        if (productPackings != null)
+        {
+            _context.productpackingpharma.RemoveRange(productPackings);
+        }
+        await _context.SaveChangesAsync();
+    }
+    public async Task<List<product>> GetProductsAsync()
+    {
+        return await _context.product.ToListAsync();
+    }
+
+    public async Task<product> GetProductByIdAsync(int id)
+    {
+        var product = await _context.product
+        .Include(p => p.packing)
+        .Include(p => p.supplierproducts) // Включаем навигационное свойство supplierproducts из модели product
+        .ThenInclude(sp => sp.supplier) // Включаем навигационное свойство supplier из модели supplierproduct
+        .Where(p => p.id == id)
+        .FirstOrDefaultAsync();
+        return product;
+    }
+
+    public async Task<List<supplier>> GetSuppliersByProductIdAsync(int productId)
+    {
+        var suppliers = await _context.suplierproduct
+            .Where(sp => sp.productid == productId)
+            .Select(sp => sp.supplier)
+            .ToListAsync();
+
+        return suppliers;
+    }
+    public async Task<List<supplier>> GetSuppliersAsync()
+    {
+        return await _context.supplier.ToListAsync();
+    }
+
+    public async Task<supplier> GetSupplierByIdAsync(int id)
+    {
+        var supplier = await _context.supplier
+            .Include(s => s.supplierproducts)
+                .ThenInclude(sp => sp.product)
+                .ThenInclude(p=>p.packing)
+                    .ThenInclude(p => p.pps)
+            .FirstOrDefaultAsync(s => s.id == id);
+
+        return supplier;
+    }
+
+    public async Task<List<product>> GetProductsBySupplierIdAsync(int supplierId)
+    {
+        var products = await _context.suplierproduct
+            .Where(sp => sp.supplierid == supplierId)
+            .Select(sp => sp.product)
+            .ToListAsync();
+
+        return products;
+    }
+
+    public async Task<productpackingsupplier> GetPackingSupplierAsync(int productId, int supplierId, int packingId)
+    {
+        return await _context.productpackingsupplier.FirstOrDefaultAsync(p =>
+            p.productid == productId && p.id == packingId);
+    }
+
+public async Task AddToPharmacyProductAsync(int productId, int pharmacyId)
+{
+    var pharmacyProduct = await _context.pharmacyproduct.FirstOrDefaultAsync(p =>
+        p.productid == productId && p.pharmacyid == pharmacyId);
+
+        if (pharmacyProduct == null || (pharmacyProduct.pharmacyid != pharmacyId && pharmacyProduct.productid != productId))
+        {
+            pharmacyProduct = new pharmacyproduct
+            {
+                productid = productId,
+                pharmacyid = pharmacyId
+            };
+            _context.pharmacyproduct.Add(pharmacyProduct);
+        }
+
+        await _context.SaveChangesAsync();
+}
+    public async Task AddToPharmacySuppliersAsync(int pharmacyId, int supplierId)
+    {
+        var pharmacySupplier = await _context.pharmacysupplier.FirstOrDefaultAsync(p=>
+        p.pharmacyid==pharmacyId && p.supplierid==supplierId);
+        if (pharmacySupplier == null || (pharmacySupplier.pharmacyid != pharmacyId && pharmacySupplier.supplierid != supplierId))
+        {
+            pharmacySupplier = new pharmacysupplier
+            {
+                pharmacyid = pharmacyId,
+                supplierid = supplierId
+            };
+            _context.pharmacysupplier.Add(pharmacySupplier);
+        }
+
+        await _context.SaveChangesAsync();
+    }
+    public async Task AddToPharmacyPackingsAsync(int productId, int packingId, int quantity)
+    {
+        var pharmacyPacking = await _context.productpackingpharma.FirstOrDefaultAsync(p =>
+    p.productid == productId && p.packingid == packingId);
+
+        if (pharmacyPacking != null)
+        {
+            pharmacyPacking.quantity += quantity;
+        }
+        else
+        {
+            pharmacyPacking = new productpackingpharma
+            {
+                quantity = quantity,
+                productid = productId,
+                packingid = packingId
+            };
+            _context.productpackingpharma.Add(pharmacyPacking);
+        }
+
+        await _context.SaveChangesAsync();
+    }
+
+    }
